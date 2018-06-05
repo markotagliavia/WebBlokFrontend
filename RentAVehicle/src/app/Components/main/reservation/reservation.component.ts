53245dc00aeeae14dbdd36de203da3263b37daee
Marko Tagliavia <max.tagliavia@gmail.com>
2018-06-05 20:28:09 +0200

    images deleted and reservation validation done

--- RentAVehicle/src/app/Components/main/reservation/reservation.component.ts
+++ RentAVehicle/src/app/Components/main/reservation/reservation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../../../Model/reservation';
+import { getLocaleDateTimeFormat } from '@angular/common';
 
 @Component({
   selector: 'app-reservation',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationComponent implements OnInit {
 
-  constructor() { }
+  errorText : string;
+  reservation : Reservation;
+
+  constructor() { 
+    this.errorText = "";
+    this.reservation = {
+      'filijalaIn' : '',
+      'filijalaOut' : '',
+      'serviceName' : '',
+      'vremeIn' : '',
+      'vremeOut' : '',
+      'adresaIn' : '',
+      'adresaOut' : '',
+       'ukupnaCena' : ''
+    }
+  }
 
   ngOnInit() {
+  }
+
+  submitRes()
+  {
+    if(this.reservation.serviceName.length == 0 || this.reservation.filijalaIn.length == 0 || this.reservation.filijalaOut.length == 0
+      || this.reservation.vremeIn.length == 0 || this.reservation.vremeOut.length == 0)
+    {
+      this.errorText = "All fields are requiered";
+      return false;
+    }
+    else
+    {
+      this.errorText = "";
+    }
   }
 
 }
