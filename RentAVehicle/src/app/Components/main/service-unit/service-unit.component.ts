1a418f39c3d9a8d848bdf481cf09aca4e99cc2d5
Marko Tagliavia <max.tagliavia@gmail.com>
2018-06-09 10:50:21 +0200

    ocene UI and logic behinde

--- RentAVehicle/src/app/Components/main/service-unit/service-unit.component.ts
+++ RentAVehicle/src/app/Components/main/service-unit/service-unit.component.ts
@@ -10,12 +10,37 @@ import { Service } from '../../../Model/service';
 export class ServiceUnitComponent implements OnInit {
 
   @Input() service : Service;
+  ocena : number;
 
   constructor() { 
-    
   }
 
   ngOnInit() {
+    this.ocena = -1;
+    if(this.service.AverageMark >= 0 && this.service.AverageMark <= 0.5)
+    {
+      this.ocena = 0;
+    }
+    else if(this.service.AverageMark >= 0.5 && this.service.AverageMark < 1.5)
+    {
+      this.ocena = 1;
+    }
+    else if(this.service.AverageMark >= 1.5 && this.service.AverageMark < 2.5)
+    {
+      this.ocena = 2;
+    }
+    else if(this.service.AverageMark >= 2.5 && this.service.AverageMark < 3.5)
+    {
+      this.ocena = 3;
+    }
+    else if(this.service.AverageMark >= 3.5 && this.service.AverageMark < 4.5)
+    {
+      this.ocena = 4;
+    }
+    else if(this.service.AverageMark >= 4.5 && this.service.AverageMark <= 5)
+    {
+      this.ocena = 5;
+    }
   }
 
 }
